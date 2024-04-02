69dfdb0f5f4c2f6e702db3636feb15def9036255
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-02 13:50:01 +0200

    Made Score class, created its attributes and now I am setting it's properties starting by player

---
+++ Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IntergalacticMission
+{
+    public class Score
+    {
+        const int MaxScore = 500;
+        const string ErrorPlayRegexMissmatch = "The player must only have alphanumeric characters on their name";
+        const string RegexPlayer = "^[a-zA-Z]+$";
+
+        private string player;
+        private string mission;
+        private uint scoring;
+
+        public string Player
+        {
+            get
+            {
+                return this.player;
+            }
+            set
+            {
+                if(!Regex.IsMatch(value, RegexPlayer))
+                {
+                    throw new Exception(ErrorPlayRegexMissmatch);
+                }
+                this.player = value;
+            }
+        }
+        
+    }
+}
