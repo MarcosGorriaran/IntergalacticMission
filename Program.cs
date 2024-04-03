bd60e1c4e623c54a2bb81a4594a7fec09929862e
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-03 12:16:04 +0200

    Finished class Score

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ public class Driver
 {
     public static void Main()
     {
+        const string AskPlayerName = "Please provide me with the name of the player {0}: ";
+        const string AskMissionCode = "Please provide me with the code of his mission: ";
+        const string AskScore = "Please provide me with the score of the mission [0-500]: ";
+        const int amountPlayers = 10;
+
 
     }
 }
--- Score.cs
+++ Score.cs
@@ -5,12 +5,23 @@ namespace IntergalacticMission
     public class Score
     {
         const int MaxScore = 500;
+        const int MinScore = 0;
         const string ErrorPlayRegexMissmatch = "The player must only have alphanumeric characters on their name";
+        const string ErrorMissionRegexMissmatch = "The mission code must start with a greek word followed by a 3 numeric code and splited by a - sign";
+        const string ErrorScoringOutOfBounds = "The value must be between 0 and 500. Both included";
         const string RegexPlayer = "^[a-zA-Z]+$";
+        const string RegexMission = "^(Alpha|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|Kappa|Lambda|Mi|Ni|Ksi|Omicron|Pi|Ro|Sigma|Tau|Ipsilon|Fi|Khi|Psi|Omega|Digamma|Heta|Qoppa|Sho|Stigma|San|Sampi)-[0-9]{3}$";
 
         private string player;
         private string mission;
-        private uint scoring;
+        private int scoring;
+
+        public Score(string playerName, string missionCode, int score) 
+        {
+            this.player = playerName;
+            this.mission = missionCode; 
+            this.scoring = score;
+        }
 
         public string Player
         {
@@ -20,7 +31,7 @@ namespace IntergalacticMission
             }
             set
             {
-                if(!Regex.IsMatch(value, RegexPlayer))
+                if (!Regex.IsMatch(value, RegexPlayer))
                 {
                     throw new Exception(ErrorPlayRegexMissmatch);
                 }
@@ -29,13 +40,30 @@ namespace IntergalacticMission
         }
         public string Mission
         {
-            get 
+            get
             {
                 return this.mission;
             }
             set
             {
-
+                if (!Regex.IsMatch(value, RegexMission))
+                {
+                    throw new Exception(ErrorMissionRegexMissmatch);
+                }
+            }
+        }
+        public int Scoring
+        {
+            get
+            {
+                return this.scoring;
+            }
+            set
+            {
+                if(value > MaxScore || value <MinScore)
+                {
+                    throw new Exception(ErrorScoringOutOfBounds);
+                }
             }
         }
         
