7725da808f212f1481a70a0a6ecb4d83247ad20e
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-03 12:29:07 +0200

    Finished making the main function

--- Program.cs
+++ Program.cs
@@ -7,8 +7,63 @@ public class Driver
         const string AskPlayerName = "Please provide me with the name of the player {0}: ";
         const string AskMissionCode = "Please provide me with the code of his mission: ";
         const string AskScore = "Please provide me with the score of the mission [0-500]: ";
+        const string FormatError = "The score must be a number";
         const int amountPlayers = 10;
 
+        Score[] scores = new Score[amountPlayers];
 
+        for(int i = 0; i < amountPlayers; i++)
+        {
+            bool error;
+            scores[i] = new Score();
+            do
+            {
+                error = false;
+                try
+                {
+                    Console.Write(AskPlayerName, i + 1);
+                    scores[i].Player = Console.ReadLine()??"";
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    error = true;
+                }
+            }while (error);
+            
+            do
+            {
+                error = false;
+                try
+                {
+                    Console.Write(AskMissionCode);
+                    scores[i].Mission = Console.ReadLine() ?? "";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    error = true;
+                }
+            } while (error);
+            do
+            {
+                error = false;
+                try
+                {
+                    Console.Write(AskScore);
+                    scores[i].Scoring = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(FormatError);
+                    error = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    error = true;
+                }
+            } while (error);
+        }
     }
 }
--- Score.cs
+++ Score.cs
@@ -4,13 +4,17 @@ namespace IntergalacticMission
 {
     public class Score
     {
-        const int MaxScore = 500;
-        const int MinScore = 0;
+        
         const string ErrorPlayRegexMissmatch = "The player must only have alphanumeric characters on their name";
         const string ErrorMissionRegexMissmatch = "The mission code must start with a greek word followed by a 3 numeric code and splited by a - sign";
         const string ErrorScoringOutOfBounds = "The value must be between 0 and 500. Both included";
         const string RegexPlayer = "^[a-zA-Z]+$";
         const string RegexMission = "^(Alpha|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|Kappa|Lambda|Mi|Ni|Ksi|Omicron|Pi|Ro|Sigma|Tau|Ipsilon|Fi|Khi|Psi|Omega|Digamma|Heta|Qoppa|Sho|Stigma|San|Sampi)-[0-9]{3}$";
+        const string DefPlayerName = "";
+        const string DefMission = "Alpha-001";
+        const int DefScore = 0;
+        const int MaxScore = 500;
+        const int MinScore = 0;
 
         private string player;
         private string mission;
@@ -21,6 +25,11 @@ namespace IntergalacticMission
             this.player = playerName;
             this.mission = missionCode; 
             this.scoring = score;
+        }
+
+        public Score():this(DefPlayerName,DefMission,DefScore)
+        {
+
         }
 
         public string Player
@@ -67,5 +76,9 @@ namespace IntergalacticMission
             }
         }
         
+        public static Score[] GenerateUniqueRanking(List<Score> scores)
+        {
+            var scores = 
+        }
     }
 }
